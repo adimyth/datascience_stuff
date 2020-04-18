fc9970105de886160479c7a06fcaf3bbd471af5f
adimyth <mishraaditya6991@gmail.com>
2020-04-18 15:33:41 +0530

    adds many things

--- optimization/differential_evolution_ackley.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from scipy.optimize import differential_evolution
-from ackley import ackley_func
-
-bounds = [(-5, 5), (-5, 5)]
-result = differential_evolution(ackley_func, bounds=bounds, maxiter=100)
-print(f"Result:\n {result}")
