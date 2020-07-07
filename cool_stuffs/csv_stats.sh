0d2d65d2d3f9aaaad31b81cd872b34dbd479fa14
adimyth <mishraaditya6991@gmail.com>
2020-07-08 01:01:04 +0530

    modifies csv_stats file to work with arbitrary file paths

--- cool_stuffs/csv_stats.sh
+++ cool_stuffs/csv_stats.sh
@@ -1,7 +1,18 @@
-for file in *.csv;
-do
-	echo $file;
-	printf "# Rows: "; awk '{print NR}' $file| tail -n 1;
-	printf "# Columns: "; head -1 $file | sed 's/[^,]//g' | wc -c;
-	printf "\n\n"
-done;
+if [ $# -eq 0 ];then
+    for file in *.csv;
+    do
+        echo $file;
+	    printf "# Rows: "; awk '{print NR}' $file| tail -n 1;
+	    printf "# Columns: "; head -1 $file | sed 's/[^,]//g' | wc -c;
+	    printf "\n\n"
+    done;
+else
+    for file in $(readlink -f $1/*.csv);
+    do
+	    echo ${file##*/}
+        printf "# Rows: "; awk '{print NR}' $file| tail -n 1;
+	    printf "# Columns: "; head -1 $file | sed 's/[^,]//g' | wc -c;
+	    printf "\n\n"
+    done;
+fi
+
